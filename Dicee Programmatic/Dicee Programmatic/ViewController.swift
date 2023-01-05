8b3eb283fd2597df92e49f9e85bf2cf4afa52e88
Jeevan Joshi <joshijeevan98@gmail.com>
2023-01-05 12:07:43 +0530

    initial commit

---
+++ Dicee Programmatic/Dicee Programmatic/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Dicee Programmatic
+//
+//  Created by Jeevan Chandra Joshi on 05/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
