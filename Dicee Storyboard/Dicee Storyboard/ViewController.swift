6039ccb8f256246b924bd717f3dd095cdd484224
Jeevan Joshi <joshijeevan98@gmail.com>
2023-01-05 19:06:32 +0530

    add app using storyboard

--- Dicee Storyboard/Dicee Storyboard/ViewController.swift
+++ Dicee Storyboard/Dicee Storyboard/ViewController.swift
@@ -8,8 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var diceImage1: UIImageView!
+    @IBOutlet var diceImage2: UIImageView!
+    @IBOutlet var player1: UILabel!
+    @IBOutlet var player2: UILabel!
+
+    var score1 = 0, score2 = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func diceRolled(_ sender: UIButton) {
+        var score = Int.random(in: 1 ... 6)
+        diceImage1.image = UIImage(named: "dice\(score)")
+        score1 += score
+        player1.text = String(score1)
+
+        score = Int.random(in: 1 ... 6)
+        diceImage2.image = UIImage(named: "dice\(score)")
+        score2 += score
+        player2.text = String(score2)
     }
 }
